fdd8f296213f1701e6c686f7667e01b86cdb33a4
CatherineLDA <Catherine.Lda4@gmail.com>
2024-11-06 02:20:45 +0800

    feat: add Firebase authentication setup and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Signin from './pages/Signin';
+import { AuthProvider } from './context/auth';
 
 function App() {
 
   return (
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/signin" element={<Signin />}/>
       </Routes>
+    </AuthProvider>
+      
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import ImageGallery from "../components/ImageGallery"
 import Navbar from "../components/Navbar"
 import UploadForm from "../components/UploadForm"
+import { useAuth } from "../hooks/useAuth"
 
 const Home = () => {
+  const { user } = useAuth();
+  console.log(user);
+
   return (
     <div className="max-w-4xl mx-auto">
       <Navbar/>
