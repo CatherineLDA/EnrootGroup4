906b5abfa107333c1b7ca11fc00621c1a3b7df81
CatherineLDA <Catherine.Lda4@gmail.com>
2024-11-06 04:57:30 +0800

    feat: initialize Supabase client

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home"
 import Signin from './pages/Signin';
 import { AuthProvider } from './context/auth';
+import PublicRoute from './routes/PublicRoute';
 
 function App() {
 
@@ -9,7 +10,11 @@ function App() {
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signin" element={
+          <PublicRoute>
+            <Signin />
+          </PublicRoute>
+        }/>
       </Routes>
     </AuthProvider>
       
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_API_KEY!;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SessionContextProvider supabaseClient={supabase}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SessionContextProvider>
   </React.StrictMode>
-)
+);
