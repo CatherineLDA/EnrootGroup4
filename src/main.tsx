74b8be92fdb6f35bf07c8eca8b0f5b897c82949c
CatherineLDA <Catherine.Lda4@gmail.com>
2024-11-07 03:21:20 +0800

    refactor: remove redundant BrowserRouter and supabaseClient initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseApiKey = import.meta.env.VITE_SUPABASE_API_KEY!;
-const supabase = createClient(supabaseUrl, supabaseApiKey);
+import { supabaseClient } from './supabase/SupabaseClient.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <App />
     </SessionContextProvider>
   </React.StrictMode>
 );
