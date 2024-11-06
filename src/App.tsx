84ba52848d6721137fdd6a477603e58ed8be2a14
CatherineLDA <Catherine.Lda4@gmail.com>
2024-11-07 03:21:37 +0800

    fix: retain SessionContextProvider and BrowserRouter for correct routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import ToteGallery from './pages/ToteGallery';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { supabaseClient } from './supabase/SupabaseClient';
 
 function App() {
-
+  
   return (
-      <Routes>
-        <Route path="/" element={<ToteGallery />}/>
-      </Routes>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tote-gallery" element={<ToteGallery />} />
+        </Routes>
+      </BrowserRouter>
+    </SessionContextProvider>
   );
 }
 
