c59a3dcc5ae522de7f670681ee4d0ab07e4175aa
CatherineLDA <Catherine.Lda4@gmail.com>
2024-11-07 03:16:57 +0800

    feat: update and style Navbar with navigation buttons for page routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 justify-between">
-      <a className="font-bold text-xl">❤️ TOTE-ally Yours</a>
-      <div className="navbar-center">
-        <button className="btn mx-2">Button 1</button>
-        <button className="btn mx-2">Button 2</button>
+    <div className="navbar w-full bg-[#5b8dc8] text-white justify-between px-5 fixed top-0 z-50 shadow-xl">
+      <a className="font-bold text-xl ">❤️ TOTE-ally Yours</a>
+      <div className="navbar-center flex items-center h-full space-x-4">
+        <Link to="/tote-gallery">
+          <button className="btn btn-ghost rounded-none h-full px-3 py-2">Tote Bag Designs</button>
+        </Link>
+        
+        <button className="btn mx-2">Event Photos</button>
       </div>
       <button>Logout</button>
     </div>
